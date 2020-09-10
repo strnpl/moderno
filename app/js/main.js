581025ce61a5b085bffd4913c8586b5643b6356e
strnpl <strnpl.de@gmail.com>
2020-09-10 12:24:38 +0300

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function(){
 
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+
+    $(".product-slider__inner").slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      dots: true,
+      arrows: false,
+    });
+
     var mixer = mixitup('.products__inner-box');
 
     
